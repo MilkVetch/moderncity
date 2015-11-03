a740537696638db5a383fb738e8b302cd6d08d65
MilkVetch <457804000@qq.com>
2015-11-04 12:13:36 +1300

    loading

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 // Description
 	console.log('Designed By: Xing.Huang');
+// Loading
+	function load() {
+		$('.loadingcss').hide();
+		$('header').show();
+		$('main').show();
+		$('footer').show();
+	}
